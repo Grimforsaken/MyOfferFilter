038cc7ec8d28545b71932507f956e63653cd3db9
Grimforsaken <grimforsaken@gmail.com>
2026-09-20 14:56:00 -0500

    Test wrapped store number on accepted screen

--- app/src/test/java/com/grimforsaken/sparkofferfilter/AcceptedShoppingScreenDetectorTest.java
+++ app/src/test/java/com/grimforsaken/sparkofferfilter/AcceptedShoppingScreenDetectorTest.java
@@ -3,7 +3,7 @@ package com.grimforsaken.sparkofferfilter;
 public final class AcceptedShoppingScreenDetectorTest {
     public static void main(String[] args) {
         String screen = "Stop #1 for Trip 8466\n2:38 PM shopping\n"
-                + "Walmart SAND SPRINGS #838\n220 S HIGHWAY 97\n"
+                + "Walmart SAND SPRINGS\n#838\n220 S HIGHWAY 97\n"
                 + "SAND SPRINGS, OK 74063-65\nCONTACT\nNAVIGATE\nCONFIRM ARRIVAL";
         require(AcceptedShoppingScreenDetector.isConfirmedShoppingTripScreen(screen),
                 "active shopping stop screen must confirm an accepted shopping order");
